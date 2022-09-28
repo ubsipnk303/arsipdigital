58555e32e05d8829d90567dd814497a91256c1c7
Agung Sasongko <agoenxz21@gmail.com>
2022-09-28 14:13:27 +0700

    tambah migration

---
+++ app/Database/Migrations/2022-09-22-090614_Arsip.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Arsip extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'        => ['type'=>'int', 'constraint'=>10, 'unsigned'=>true, 'auto_increment'=>true],
+            'nomor'     => ['type'=>'varchar', 'constraint'=>25, 'null'=>true],
+            'judul'     => ['type'=>'varchar', 'constraint'=>255, 'null'=>true],
+            'ringkasan' => ['type'=>'text',  'null'=>true],
+            'tgl'       => ['type'=>'date', 'null'=>true],
+            'kategori_id'   => ['type'=>'int', 'constraint'=>10, 'null'=>true, 'unsigned'=>true],
+            'pengguna_id'   => ['type'=>'int', 'constraint'=>10, 'null'=>true, 'unsigned'=>true],
+            'created_at'    => ['type'=>'datetime', 'null'=>true],
+            'updated_at'    => ['type'=>'datetime', 'null'=>true],
+            'deleted_at'    => ['type'=>'datetime', 'null'=>true],
+
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('kategori_id', 'kategori', 'id', 'cascade', 'set null');
+        $this->forge->addForeignKey('pengguna_id', 'pengguna', 'id', 'cascade'); 
+        $this->forge->createTable('arsip');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('arsip');
+    }
+}
